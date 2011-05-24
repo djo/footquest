68587d391c7b6b1e1c3ca56eb4441a56c43dec46
Andrew Djoga <ad@anahoret.com>
2011-05-24 17:29:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footquest::Application.config.secret_token = 'c38cba9362cd5387e3c7327d52a72a90fc77a6f8ad5c5885ba031bdfd54997dc21c4ce3aec548350acf3b47c700ea470c3fc481ba892520c09fe634558b0f37a'
