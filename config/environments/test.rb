58d25ac1b1f3956d417ec3d164374bbb94b0e304
Andrew Djoga <ad@anahoret.com>
2011-05-24 18:07:07 +0300

    Set up default urls

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Footquest::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  config.action_mailer.default_url_options = { :host => 'test.localhost:3000' }
 end
