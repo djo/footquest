e23312079c3966f4b0e0dcf97aa18f7d71f8c590
Andrew Djoga <ad@anahoret.com>
2011-06-15 14:41:12 +0300

    Invitation email is delivers on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < InheritedResources::Base
   load_and_authorize_resource
   
   before_filter :build_user, :only => :create
+  
+  def create
+    create! do |success, failure|
+      success.html { send_invitation and redirect_to(users_url, :notice => 'Приглашение отослано') }
+    end
+  end
 
   private
   
@@ -11,5 +17,9 @@ class UsersController < InheritedResources::Base
     @user.attributes = params[:user]
     @user.password = Digest::SHA1.hexdigest(Time.now.to_s)[0,8]
     @user.password_confirmation = @user.password
+  end
+  
+  def send_invitation
+    UserMailer.invitation_email(@user).deliver
   end
 end
