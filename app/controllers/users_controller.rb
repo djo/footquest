31b3329f2852673887672c8414b4b7598f402708
Andrew Djoga <ad@anahoret.com>
2011-06-15 14:36:54 +0300

    Generates password in the user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < InheritedResources::Base
   load_and_authorize_resource
+  
+  before_filter :build_user, :only => :create
+
+  private
+  
+  def build_user
+    @user = User.new
+    @user.accessible = current_user.role
+    @user.attributes = params[:user]
+    @user.password = Digest::SHA1.hexdigest(Time.now.to_s)[0,8]
+    @user.password_confirmation = @user.password
+  end
 end
