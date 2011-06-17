31d4021f4ae6f923cdcbea15e884679343873f3c
Andrew Djoga <ad@anahoret.com>
2011-06-17 13:32:47 +0300

    Quest#update_user_quests were completed

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -10,6 +10,18 @@ class Quest < ActiveRecord::Base
   validates_numericality_of :cost, :greater_than => 0.01, :allow_blank => true
   
   def update_user_quests(user_ids)
+    ids = user_ids.map &:to_i
+    remove_user_quests(self.user_ids - ids)
+    create_user_quests(ids - self.user_ids)
+  end
+  
+  private
+  
+  def remove_user_quests(user_ids)
+    UserQuest.delete_all(:quest_id => id, :user_id => user_ids)
+  end
+  
+  def create_user_quests(user_ids)
     user_ids.each { |id| user_quests.create!(:user_id => id)  }
   end
 end
--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -5,10 +5,19 @@ describe Quest do
     let(:quest) { Factory :quest }    
     let(:first_user) { Factory :user }
     let(:second_user) { Factory :user }
+    let(:third_user) { Factory :user }
     
     it "should create user quests" do
       expect { quest.update_user_quests([first_user.id, second_user.id]) }.
         to change(UserQuest, :count).from(0).to(2)
+    end
+
+    it "should update user quests" do
+      quest.users << first_user
+      quest.users << second_user
+      
+      quest.update_user_quests [first_user.id, third_user.id]
+      quest.reload.users.should == [first_user, third_user]
     end
   end
 end
