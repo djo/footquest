b640aba3e5f58969185b461a8e083bd6c809260e
Andrew Djoga <ad@anahoret.com>
2011-06-22 18:06:00 +0300

    Created metadata helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def active_on(name)
     'active' if controller_name == name
   end
+  
+  # Adds custom stylesheet from a template
+  def stylesheet(*args)
+    content_for(:head) { stylesheet_link_tag(*args) }
+  end
+
+  # Adds custom javascript file from a template
+  def javascript(*args)
+    content_for(:head) { javascript_include_tag(*args) }
+  end
 end
