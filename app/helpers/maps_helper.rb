e79ce9b652988c436cad2955331c873964546b56
Andrew Djoga <ad@anahoret.com>
2011-07-27 17:25:44 +0300

    Displays map with predefined options

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,6 +1,15 @@
 module MapsHelper
   # Test api key
   def api_map_source
-    "http://api-maps.yandex.ru/1.1/index.xml?key=ANpUFEkBAAAAf7jmJwMAHGZHrcKNDsbEqEVjEUtCmufxQMwAAAAAAAAAAAAvVrubVT4btztbduoIgTLAeFILaQ=="    
+    "http://api-maps.yandex.ru/1.1/index.xml?key=ANpUFEkBAAAAf7jmJwMAHGZHrcKNDsbEqEVjEUtCmufxQMwAAAAAAAAAAAAvVrubVT4btztbduoIgTLAeFILaQ=="
+  end
+
+  def map_options
+    if resource
+      { :lng => resource.lng, :lat => resource.lat, :zoom => resource.zoom }
+    else
+      # Default options for Donetsk city
+      { :lng => 37.804478, :lat => 48.002037, :zoom => 12 }
+    end
   end
 end
--- public/javascripts/views/maps/map.js
+++ public/javascripts/views/maps/map.js
@@ -1,4 +1,4 @@
-$(function () {
+var initMap = function (options) {
   var map = new YMaps.Map(YMaps.jQuery("#map")[0]);
-  map.setCenter(new YMaps.GeoPoint(37.804478,48.002037), 12);
-});
+  map.setCenter(new YMaps.GeoPoint(options.lng, options.lat), options.zoom);
+}
